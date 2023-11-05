7fc8c71d89e0b7e2264d6311719973eb9026c680
Abbas Gheydi <abbas.gheydi@gmail.com>
2023-11-05 18:04:16 +0330

    fix reconcile bug

--- controllers/httpproxy_controller.go
+++ controllers/httpproxy_controller.go
@@ -29,7 +29,6 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 var snapShotVersion int
@@ -85,6 +84,6 @@ func (r *HTTPProxyReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 func (r *HTTPProxyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&contourv1.HTTPProxy{}).
-		WithEventFilter(predicate.GenerationChangedPredicate{}).
+		//WithEventFilter(predicate.GenerationChangedPredicate{}).
 		Complete(r)
 }
