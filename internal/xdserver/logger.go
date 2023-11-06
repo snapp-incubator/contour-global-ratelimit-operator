24a1e980b3d3d5a4f3ba46bc0ffe05385e29aba9
Abbas Gheydi <abbas.gheydi@gmail.com>
2023-11-06 15:06:15 +0330

    fix logs problem

--- internal/xdserver/logger.go
+++ internal/xdserver/logger.go
@@ -16,18 +16,18 @@ type Logger struct {
 // Debugf logs a debug message if Debug is true.
 func (logger Logger) Debugf(format string, args ...interface{}) {
 	if logger.Debug {
-		log.Printf("\t\tDEBUG\t"+format+"\n", args...)
+		log.Printf("\t\tDEBUG\t"+format+"%v\n", args...)
 	}
 }
 
 // Infof logs an informational message.
 func (logger Logger) Infof(format string, args ...interface{}) {
-	log.Printf("\t\tINFO\t"+format+"\n", args...)
+	log.Printf("\t\tINFO\t"+format+"%v\n", args...)
 }
 
 // Warnf logs a warning message.
 func (logger Logger) Warnf(format string, args ...interface{}) {
-	log.Printf("\t\tWARN\t"+format+"\n", args...)
+	log.Printf("\t\tWARN\t"+format+"%v\n", args...)
 }
 
 // Errorf logs an error message.
